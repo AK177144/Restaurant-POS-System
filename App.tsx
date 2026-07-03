83f495973f5bfbc1bcbb064e4375e9daff4e4a70
Anandu K <ananduk2005@gmail.com>
2026-07-03 15:19:03 +0530

    Fix Supabase environment configuration

--- App.tsx
+++ App.tsx
@@ -1,20 +1,5 @@
-import { View, Text } from "react-native";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 20,
-      }}
-    >
-      <Text>URL:</Text>
-      <Text>{String(process.env.EXPO_PUBLIC_SUPABASE_URL)}</Text>
-
-      <Text style={{ marginTop: 20 }}>KEY EXISTS:</Text>
-      <Text>{String(!!process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY)}</Text>
-    </View>
-  );
+  return <AppNavigator />;
 }
