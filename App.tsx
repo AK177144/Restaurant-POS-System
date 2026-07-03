55b731ac0129822628a1c7a6b9c81dd540586541
Anandu K <ananduk2005@gmail.com>
2026-07-03 14:35:32 +0530

    Test Supabase release fix

--- App.tsx
+++ App.tsx
@@ -1,5 +1,20 @@
-import AppNavigator from "./src/navigation/AppNavigator";
+import { View, Text } from "react-native";
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20,
+      }}
+    >
+      <Text>URL:</Text>
+      <Text>{String(process.env.EXPO_PUBLIC_SUPABASE_URL)}</Text>
+
+      <Text style={{ marginTop: 20 }}>KEY EXISTS:</Text>
+      <Text>{String(!!process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY)}</Text>
+    </View>
+  );
 }
